891f09ba0d7b1641e8070f5809e98e801d07c6e6
tincher <joel.ewig@gmail.com>
2023-08-16 11:19:48 +0300

    feat: Add multiple moves

--- game/main.py
+++ game/main.py
@@ -9,23 +9,26 @@ class Game(object):
     def __init__(self):
         self.top_state = State(TOP)
         self.bottom_state = State(BOT)
-        self.top = True
+        self.top_turn = True
 
     def get_valid_moves(self):
         pass
 
-    def get_move_result(self, move):
-        state = self.top_state if move.top else self.bottom_state
+    def make_moves(self, move):
+        if move.top_turn != self.top_turn:
+            print("WARNING: move turn is not same as game turn")
+        state = self.top_state if self.top_turn else self.bottom_state
         move_field = move.field
-        if (current_count := state[move.field]) < 2:
-            raise ValueError(f"Invalid Move: There are not enough gems in the pit!"
+        if (current_count := state.state[move.field]) < 2:
+            raise ValueError(f"Invalid Move: There are not enough stones in the pit!"
                              f"pit: {move.field}, entry: {state[move.field]}",
                              str(self))
 
-        while current_count > 0:
-            # fill pits, check for ring
-            # check last, redo
-            state.make_move(move_field)
+        while current_count > 1:
+            move_field = state.make_move(move_field)
+            current_count = state.state[move_field]
+
+        self.top_turn = not self.top_turn
 
     def get_game_result(self):
         pass
@@ -37,9 +40,9 @@ class Game(object):
 
         result = [entry.replace("[", "").replace("]", "").strip()
                   for entry in result]
-        result.insert(2, '-' * len(result[0]))
-        result.append("TOP" if self.top else "BOT")
-        result = '\n'.join(result)
+        result.insert(2, '-' * 10)
+        result.append("TOP" if self.top_turn else "BOT")
+        result = '\n'.join(result) + "\n"
         return result
 
     def __repr__(self):
@@ -69,7 +72,7 @@ class State(object):
         self.state[field] = 0
         self.state[field + 1:field + stone_count + 1] += 1
         if last_field < field:
-            self.state[:last_field] += 1
+            self.state[:last_field+1] += 1
         return last_field
 
     def get_representation(self):
@@ -83,9 +86,9 @@ class State(object):
 
 class Move(object):
 
-    __slots__ = ["top", "field"]
+    __slots__ = ["top_turn", "field"]
 
-    def __init__(self, top, field):
+    def __init__(self, top_turn, field):
         """Init a new move
 
         Parameters
@@ -95,7 +98,7 @@ class Move(object):
         field : int
             which field the move starts with (counting from bottom-left from player-view in play-direction)
         """
-        self.top = top
+        self.top_turn = top_turn
         self.field = field
 
 

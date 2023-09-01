706abafce3881aa34f1ac6f95eeed5f546185a5b
tincher <joel.ewig@gmail.com>
2023-09-01 16:24:29 +0200

    feat: Add devices for optimization

--- td_stones/optimize.py
+++ td_stones/optimize.py
@@ -5,10 +5,14 @@ from td_stones.train import main
 import skopt
 from argparse import ArgumentParser
 from tqdm import tqdm
+import torch
 
 
 def optimize(iterations, game_count):
-    # discount, alpha, hidden_units
+    default_device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    torch.set_default_device(default_device)
+    print(torch.tensor([1.2, 3]).device)
+
     space = []
     space.append(Real(name="discount", prior="uniform", low=0.01, high=0.99))
     space.append(Real(name="alpha", prior="log-uniform", low=0.001, high=0.9))
